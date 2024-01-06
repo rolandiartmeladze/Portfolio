6c851b17b38da8ac4b692c09adddd12e54612d7a
rolandiartmeladze <rartmeladze@gmail.com>
2024-01-07 02:25:32 +0400

    Second commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,42 @@
 import React from "react";
+import styled from 'styled-components';
 import { useState, useEffect } from "react";
 import findIcon from '../icon/find.png';
 import findClose from '../icon/close.png';
 import menu from '../icon/menu.png';
 import menuClose from '../icon/menu-close.png';
-import { Console } from "console";
+
+const HeadDiv = styled.div`
+        position: absolute;
+        justify-content: center;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+    `;
+    const HeaderItem = styled.div`
+            box-shadow: 0px 2px 3px 0px rgb(255, 255, 255, 0.3);
+            width: 90%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            margin-top: -60px;
+            transition: 0.7s ease;
+        `;
+        const FindInput = styled.input`
+                height: 30px;
+                width: 120px;
+                border-radius: 8px;
+                outline: none;
+            `;
 
 export default function Header(){
 
     const [clickFinde, setClickFinde] = useState(false);
-    const [clickMenu, setClickMenu] = useState(false);
+    const FindClick =()=>{ clickFinde?setClickFinde(false):setClickFinde(true) }
 
-    const FindClick =()=>{
-        clickFinde?setClickFinde(false):setClickFinde(true)
-    }
-    const MenuClick =()=>{
-        clickMenu?setClickMenu(false):setClickMenu(true)
-    }
-
-
+        const [clickMenu, setClickMenu] = useState(false);
+        const MenuClick =()=>{ clickMenu?setClickMenu(false):setClickMenu(true) }
 
     useEffect(()=>{
         let header = document.getElementById('header');  
@@ -32,56 +50,25 @@ export default function Header(){
   
     return (
       <>
-        <div 
-        id="header"
-          style={{
-            boxShadow: "0px 2px 3px 0px rgb(255, 255, 255, 0.3)",
-            width: "90%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            position: "relative",
-            marginTop:'-60px',
-            transition: '0.7s ease'
-          }}
-        >
-          <div
-            style={{
-              position: "absolute",
-              left: "5px",
-              justifyContent: "center",
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
+        <HeaderItem id="header">
+          <HeadDiv style={{left: "5px"}}>
             {clickFinde ? (
               <>
-                <input
-                  style={{
-                    height: "30px",
-                    width: "120px",
-                    borderRadius: "8px",
-                    outline: "none",
-                  }}
+                <FindInput 
                   type="text"
                 />
-                <img
-                  onClick={FindClick}
+                <img onClick={FindClick}
                   style={{ width: "25px", marginLeft: '4px' }}
                   src={findClose}
-                  alt="findIcon"
-                />
+                  alt="findIcon" />
               </>
             ) : (
-              <img
-                onClick={FindClick}
+              <img onClick={FindClick}
                 style={{ width: "30px" }}
                 src={findIcon}
-                alt="findClosIcon"
-              />
+                alt="findClosIcon" />
             )}
-          </div>
+          </HeadDiv>
           <h1
             style={{
               padding: "5px",
@@ -93,33 +80,22 @@ export default function Header(){
             You are welcome
           </h1>
 
-          <div
-            style={{
-              position: "absolute",
-              right: "15px",
-              justifyContent: "center",
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
+          <HeadDiv style={{right: "15px"}}>
+
             {clickMenu ? (
-              <img
-                onClick={MenuClick}
+              <img onClick={MenuClick}
                 style={{ width: "25px" }}
                 src={menuClose}
-                alt="menuIcon"
-              />
+                alt="menuIcon" />
             ) : (
-              <img
-                onClick={MenuClick}
+              <img onClick={MenuClick}
                 style={{ width: "30px" }}
                 src={menu}
-                alt="menuClosIcon"
-              />
+                alt="menuClosIcon" />
             )}
-          </div>
-        </div>
+
+          </HeadDiv>
+        </HeaderItem>
       </>
     );
 };
