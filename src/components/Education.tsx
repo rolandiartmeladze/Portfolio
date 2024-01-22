b3b8f7b7fece7b11a2dcb78bc40595057d2a3558
rolandiartmeladze <rartmeladze@gmail.com>
2024-01-22 22:48:33 +0400

    education commit

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -49,7 +49,8 @@ const Education: React.FC<abouteme> = ({ismobil}) => {
     // backgroundColor: 'green',
     flexDirection: 'row',
     alignItems: 'flex-start',
-    justifyContent: 'center'
+    paddingLeft:'5%',
+    justifyContent: 'flex-start'
     }}>
         <div style={{backgroundColor: 'white' , width: '70px', padding:'3px', borderRadius: '50%', marginTop:'20px'}}>
             <img style={{width: '100%'}} src={stulogo} alt="eduLogo"/></div>
@@ -83,29 +84,21 @@ const Education: React.FC<abouteme> = ({ismobil}) => {
     // backgroundColor: 'green',
     flexDirection: 'row',
     alignItems: 'flex-start',
-    justifyContent: 'center'
+    paddingLeft:'5%',
+    justifyContent: 'flex-start'
     }}>
         <div style={{backgroundColor: 'white' , width: '70px', padding:'3px', borderRadius: '50%', marginTop:'20px'}}>
             <img style={{width: '100%'}} src={codehslogo} alt="eduLogo"/></div>
         <ul style={{listStyle:'none', padding:'10px', margin:'5px', maxWidth: '70%', textAlign:'left'}}>
-        <h1 style={{margin:'5px'}}>The Georgian Technical University (GTU)</h1>
+        <h1 style={{margin:'5px'}}> codehs.com </h1>
 
-                <strong style={{color:'yellow', margin:'4px'}}>Bachelor's degree 2015 - 2019 y.</strong>
+                <strong style={{color:'yellow', margin:'4px'}}>Introduction to Computer Science in JavaScript</strong>
 
-           <li> 
-           <strong>Information Technology (IT): </strong>
-            This program typically covers a wide range of topics such as.
-            </li>
+           <li>JavaScript Programming</li>
 
-                <li>
-               <strong> Computer Engineering:</strong> 
-                 Design and development of computer systems and hardware.
-                </li>
+                
 
-                    <li> 
-                    <strong>Network Engineering or Networking:</strong> 
-                      Programs in networking focus on the design, implementation, and management of computer networks.
-                    </li>
+                    
 
                 </ul>
     </article>
