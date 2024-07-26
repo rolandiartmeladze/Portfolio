e448790a3bcc224014026ba6873a068e82b706e5
rolandiartmeladze <rartmeladze@gmail.com>
2024-07-27 00:37:18 +0400

    head optimise II

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,13 +7,15 @@ import { MdEmail } from "react-icons/md";
 
 const Header = () => {
 
+    const mobile = window.innerWidth <= 768;
+    
     return (
 
         <nav>
             <ul>
-            <a href="mailto: rartmeladze@gmail.com"> <MdEmail /><li>Rartmeladz@gmail.com</li></a>
-            <a href="tel: +995595035668"> <FaPhoneSquareAlt /><li>(+995) 595 03-56-68</li> </a>
-            <a href="https://www.linkedin.com/in/roland-a-96240487"> <FaLinkedin /><li>Roland Artmeladze</li></a>
+            <a href="mailto: rartmeladze@gmail.com"> <MdEmail /><li>{mobile? 'Write Email' : 'Rartmeladz@gmail.com'}</li></a>
+            <a href="tel: +995595035668"> <FaPhoneSquareAlt /><li>{mobile? 'Call Now' : '(+995) 595 03-56-68'}</li> </a>
+            <a href="https://www.linkedin.com/in/roland-a-96240487"> <FaLinkedin /><li>{mobile? 'Show Profile' : 'Roland Artmeladze'}</li></a>
             </ul>
             
         </nav>
