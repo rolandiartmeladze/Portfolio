9e5f9fda9d23ef43d22c2e9911db6ed533d4aaeb
rolandiartmeladze <rartmeladze@gmail.com>
2024-07-27 01:12:13 +0400

    pdate p element UI with mobile

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -54,13 +54,36 @@ const Summary =()=>{
 
     return(
         <p>
-            As a beginner full-stack web developer, I'm driven by the opportunity to create meaningful solutions from 
+            {/* As a beginner full-stack web developer, I'm driven by the opportunity to create meaningful solutions from 
             scratch or tackle practical challenges head-on. My expertise lies in frontend development, where I use <b>HTML</b>, 
             <b>CSS</b>, <b>JavaScript</b>, and <b>React</b> to bring user visions to life with precision and style. Recently, 
             I've embraced <b>TypeScript</b> for enhanced code quality and scalability. On the backend, I've successfully 
             developed <b>Node.js</b> <b>Express</b> applications, building reliable server-side solutions. Additionally, I proficiently 
             manage databases using <b>MongoDB</b> and <b>Mongoose Atlas</b> for seamless data integration. I thrive on 
-            pushing boundaries and exploring emerging technologies to deliver effective results in collaborative environments.
+            pushing boundaries and exploring emerging technologies to deliver effective results in collaborative environments. */}
+       
+       
+            As a passionate beginner <b>full-stack web developer</b>, I am driven by the excitement of creating meaningful 
+            solutions from the ground up and addressing real-world challenges head-on. My expertise lies 
+            predominantly in frontend development, where I leverage <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, and <b>React</b> to 
+            transform user visions into engaging, responsive, and visually appealing web applications. 
+            I recently incorporated TypeScript into my toolkit, which has significantly enhanced 
+            my code quality and scalability, allowing me to build more robust and maintainable applications. 
+            <br/>
+
+            On the backend, I've honed my skills in developing efficient and reliable server-side solutions 
+            using <b>Node.js</b>, <b>Express</b> and <b>PHP (enter levet)</b>. My experience includes designing and implementing RESTful APIs, 
+            handling complex business logic, and ensuring seamless communication between the client 
+            and server. Furthermore, I proficiently manage databases using <b>MongoDB</b> and <b>Mongoose Atlas</b>, 
+            ensuring smooth data integration and consistency across applications.
+            <br/>
+
+            I thrive on pushing boundaries and continuously exploring emerging technologies to stay at the 
+            forefront of web development trends. My goal is to deliver effective and innovative results in 
+            collaborative environments, where I can contribute my technical skills and creative problem-solving 
+            abilities to build exceptional products. Whether working independently or as part of a team, I am 
+            committed to continuous learning and growth, always seeking new challenges that will allow me to 
+            expand my knowledge and make a meaningful impact.
         </p>
     );
 
