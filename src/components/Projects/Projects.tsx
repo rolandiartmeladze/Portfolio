3cb0689e061a3906459ec7d9f554f60ac03fa979
rolandiartmeladze <rartmeladze@gmail.com>
2024-08-06 22:15:15 +0400

    update in projects component API reques with Backend / tacke githube reposytories info

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,15 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import './Projects.css'
 
-
-const Projects = ()=>{
-
+interface Repo {
+    name: string;
+  }
+  
+  interface Props {
+    repos: Repo[];
+  }
+  
+  const Projects: React.FC = () => {
+    const [data, setData] = useState<Props | null>(null);
+  
+    useEffect(() => {
+      fetch('http://127.0.0.1:5000/api/projects')
+        .then(response => response.json())
+        .then(data => setData({ repos: data }));
+    }, []);
+  
+    if (data) {
+      data.repos.map((repo: Repo) => console.log(repo.name));
+    }
+  
     return(
         <section className="Projects-container">
-            <h1 style={{}}>
+            <h1 style={{    display: 'flex', float:'none'}}>
                 Projects
             </h1>
+
+            <div>
+
+                <h4>
+                    reposytory  form Githube API
+                </h4>
+
+                <ul style={{flexDirection: 'column', alignItems: 'flex-start'}}>
+
+                    {
+                        data?.repos.map((repo:Repo)=>(
+                            <li>
+                                {repo.name}
+                            </li>
+                        ))
+                    }
+                </ul>
+                </div>
         </section>
     );
 
