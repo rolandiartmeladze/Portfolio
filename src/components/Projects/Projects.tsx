9677f12015cd7662df6a671216efd6694cc9b19c
rolandiartmeladze <rartmeladze@gmail.com>
2024-08-07 12:00:35 +0400

    filter my githube repositories by name show only selected reposytory

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,6 +4,7 @@ import './Projects.css'
 
 interface Repo {
     name: string;
+    languages_url:string;
   }
   
   interface Props {
@@ -17,12 +18,32 @@ interface Repo {
       fetch('http://127.0.0.1:5000/api/projects')
         .then(response => response.json())
         .then(data => setData({ repos: data }));
+
+
     }, []);
   
     if (data) {
       data.repos.map((repo: Repo) => console.log(repo.name));
     }
   
+
+    const RepositoriesName =[
+        'E-Commerce-Application_Frontend',
+        'Portfolio',
+        'DataCount',
+        'TravelInGeorgia_Test',
+        'Vacation-In-Georgia',
+        'PongGame',
+        'BreackOutGame_Test',
+        'PortfolioBackend',
+        'E-Commerce-Application_Backend'
+
+    ];
+
+
+    const selectedRepositories =  data?.repos.filter((repo: Repo)=> RepositoriesName.includes(repo.name));
+
+
     return(
         <section className="Projects-container">
             <h1 style={{    display: 'flex', float:'none'}}>
@@ -38,10 +59,16 @@ interface Repo {
                 <ul style={{flexDirection: 'column', alignItems: 'flex-start'}}>
 
                     {
-                        data?.repos.map((repo:Repo)=>(
-                            <li>
+                        selectedRepositories?.map((repo:Repo)=>(
+                            
+
+                            
+                            <li style={{display: 'flex', flexDirection: 'row'}}>
                                 {repo.name}
+                               <a target="_blank" style={{color: 'red'}} href={`${repo.languages_url}`}>languages link</a>                   
+
                             </li>
+                            
                         ))
                     }
                 </ul>
