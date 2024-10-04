39d3880d8ca83a781c321bc34e5edc8108892a44
rolandiartmeladze <rartmeladze@gmail.com>
2024-10-04 20:35:50 +0400

    update code remove old detalies components or styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import { BrowserRouter as Router, useNavigation, Routes, Route, useNavigate, Lin
 
 import './App.css';
 
-import Header from './components/Header';
+// import Header from './components/Header';
 import Header1 from './components/Header/Header';
-import Title from './components/Title';
-import Photo from './components/Photo';
-import AboutMe from './components/AboutMe';
-import GithubRepository from './components/Repositories';
-import Education from './components/Education';
-import Experience from './components/Experience';
-// import Projects from './components/Projects';
-import Repostest from './components/Repotest';
-import Contact from './components/Contact';
-import Skills from './components/Skills';
+// import Title from './components/Title';
+// import Photo from './components/Photo';
+// import AboutMe from './components/AboutMe';
+// import GithubRepository from './components/Repositories';
+// import Education from './components/Education';
+// import Experience from './components/Experience';
+// // import Projects from './components/Projects';
+// import Repostest from './components/Repotest';
+// import Contact from './components/Contact';
+// import Skills from './components/Skills';
 
 
 import Home from './components/Home/Home';
@@ -78,23 +78,7 @@ function App() {
 
 
 
-<Route element = {<>
-      {/* <Header /> */}
-     <Link to={'/'}> <Photo /></Link>
-      <Title  ismobil={ismobil} />
 
-
-
-      <AboutMe ismobil={ismobil} />
-      <Education  ismobil={ismobil} />
-      <Experience ismobil={ismobil} />
-      {/* <Projects />       */}
-      <Skills />
-
-      <Contact />
-            {/* <GithubRepository />
-<Repostest /> */}
-  </>} />
 
 </Routes>
 
