b9df62b3df67ace63d2be755626462c25eb8dbd6
rolandiartmeladze <rartmeladze@gmail.com>
2024-11-30 21:59:12 +0400

    Refactor Header component: optimize icons and contact rendering; replace Link with Button component for Blog

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,44 +1,47 @@
 import { Link } from "react-router-dom";
+import Button from "../Ui/button";
+import { Icons } from "../Icons";
 
 const contact = [
   {
     href: "Rartmeladz@gmail.com",
     label: "Rartmeladz@gmail.com",
     label2: "Write Email",
-    icon: "MdEmail",
+    icon: "gmail",
   },
   {
     href: "(+995) 595 03-56-68",
     label: "(+995) 595 03-56-68",
     label2: "Call Now",
-    icon: "FaPhoneSquareAlt",
+    icon: "phone",
   },
   {
     href: "https://www.linkedin.com/in/roland-a-96240487",
     label: "Roland Artmeladze",
     label2: "Show Profile",
-    icon: "FaLinkedin",
+    icon: "linkedin",
   },
 ];
 
 const Header = () => {
   const mobile = window.innerWidth <= 768;
   return (
-    <nav className="flex w-full flex-row justify-between py-2.5 px-10">
+    <nav className="flex w-full flex-row items-center justify-between py-2.5 px-10">
       <ul className="flex flex-row gap-2.5">
-        {contact.map((item, index) => (
-          <li key={index} className="text-pink-50">
-            <Link to={item.href}> {mobile ? item.label : item.label2} </Link>
-          </li>
-        ))}
+        {contact.map((item, index) => {
+          const Icon = Icons[item.icon as keyof typeof Icons];
+          return (
+            <li key={index} className=" flex gap-1 items-center text-pink-50">
+              <Icon className="size-6" />
+              <Link to={item.href}> {!mobile ? item.label : item.label2} </Link>
+            </li>
+          );
+        })}
       </ul>
-      <Link to={"/Blog"}>
-        {" "}
-        <samp className="text-yellow-50 bg-stone-400 py-2 px-4 rounded-md">
-          {" "}
-          Blog{" "}
-        </samp>
-      </Link>
+
+      <Button to="/Blog" size="sm">
+        Blog
+      </Button>
     </nav>
   );
 };
