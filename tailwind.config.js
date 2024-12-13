b31713fa79f25aacab968a69bfb77901be85f6f2
rolandiartmeladze <rartmeladze@gmail.com>
2024-12-13 21:54:23 +0400

    chore(tailwind): add primaryYellow and border default settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryYellow: "#FFCC00",
+      },
+      borderColor: {
+        default: "rgb(255,170,0)",
+      },
+      borderOpacity: {
+        defoult: '1',
+      },
+      textColor: {
+        primary: "#FFCC00",
+        defoult: "rgb(500,250,255)",
+      }
+    },
   },
   plugins: [],
-}
-
+};
