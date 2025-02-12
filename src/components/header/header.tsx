0b2fe265705551cac24675d072d693b27e690e04
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-12 07:01:22 -0800

    updare hearde component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import ContactButtons from "./contact-buttons";
+import MenuButton from "./menu-button";
+
 export default function Header() {
-    return(
-        <nav>
-            <ul>
-                <li>
-                    Home
-                </li>
-            </ul>
-        </nav>
-    );
-};
+  return (
+    <header className="px-6  py-1 flex justify-between bg-secondary border-b border-borderColor lg:px-12">
+      <ContactButtons />
+      <div className="flex justify-between gap-2">
+        <Button variant="default" asChild>
+          <Link href={"./blog"}>Blog</Link>
+        </Button>
+        <MenuButton />
+      </div>
+    </header>
+  );
+}
