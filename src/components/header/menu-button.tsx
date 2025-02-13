3aade962b7cd5cbb39a694f786815d55674558dc
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-13 02:55:52 -0800

    update mobile menu Button

--- src/components/header/menu-button.tsx
+++ src/components/header/menu-button.tsx
@@ -1,10 +1,10 @@
-import { Button } from "../ui/button";
-import { Icons } from "../ui/icons";
+import { SheetTrigger } from "@/components/ui/sheet";
+import { Icons } from "../icons";
 
-export default function MenuButton () {
-    return(
-        <Button className="flex cursor-pointer lg:hidden" asChild variant="link" size="icon" >
-            <Icons.menu className="size-12" />
-        </Button>
-    );
-};
+export default function MenuButton() {
+  return (
+    <SheetTrigger className="flex cursor-pointer lg:hidden">
+      <Icons.menu className="size-12" />
+    </SheetTrigger>
+  );
+}
