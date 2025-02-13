8bac16531dbdf93e4f5ac886079930368a38bf1e
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-13 03:01:47 -0800

    update header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import ContactButtons from "./contact-buttons";
-import MenuButton from "./menu-button";
+import MobileMenu from "./mobile-menu"; 
 
 export default function Header() {
   return (
-    <header className="px-6  py-1 flex justify-between bg-secondary border-b border-borderColor lg:px-12">
+    <header className="px-6 py-3 flex justify-between bg-secondary border-b border-borderColor lg:px-12">
       <ContactButtons />
       <div className="flex justify-between gap-2">
-        <Button variant="default" asChild>
-          <Link href={"./blog"}>Blog</Link>
+        <Button variant="default" asChild className="rounded-md">
+          <Link href="/blog" className="text-base">Blog</Link>
         </Button>
-        <MenuButton />
+        <MobileMenu />
       </div>
     </header>
   );
