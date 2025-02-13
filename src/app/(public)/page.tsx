efaa0d5dfa68933ba66fb35ca7f4cb91dc439055
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-13 12:53:16 -0800

    update page & sheet components

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,11 @@
+import Navigate from "@/components/navigation/navigation";
 import AoutMe from "@/components/sections/about";
 
 
 export default function IndexPage() {
   return (
     <main>
+      <Navigate />
       <AoutMe />
     </main>
   );
