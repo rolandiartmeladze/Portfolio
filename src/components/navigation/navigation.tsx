4449d6af8aa33c286d6ea995adade066bd889dca
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-13 12:51:53 -0800

    add navigation component

---
+++ src/components/navigation/navigation.tsx
@@ -0,0 +1,9 @@
+import MenuList from "./menu-list";
+
+export default function Navigate() {
+  return (
+    <nav className="hidden absolute top-0 right-12 h-full lg:flex items-center p-4 transition-transform md:translate-x-0">
+      <MenuList />
+    </nav>
+  );
+}
