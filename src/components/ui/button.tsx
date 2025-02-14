b9465a2808ae36f7012a86fb1d89e14c0a8eddd8
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-13 23:56:45 -0800

    update button component style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-muted bg-background shadow-sm hover:bg-primary hover:text-accent-foreground duration-150",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
