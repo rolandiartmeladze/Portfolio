6295ff4fcae0507dfeceb11fa5c6be9c8f4b6ab1
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-17 11:57:20 -0800

    ref/update header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import ContactButtons from "./contact-buttons";
-import MobileMenu from "./mobile-menu"; 
+import MobileMenu from "./mobile-menu";
 import ThemeToggle from "./theme-toggle";
 
 export default function Header() {
   return (
-    <header className="px-6 py-3 flex justify-between bg-secondary border-b border-borderColor lg:px-12">
+    <header className="px-4 py-3 flex justify-between bg-secondary border-b border-borderColor lg:px-12">
       <ContactButtons />
-      <div className="flex justify-between gap-2">
-        <Button variant="default" asChild className="rounded-md">
-          <Link href="/blog" className="text-base">Blog</Link>
+      <div className="flex justify-between  gap-2">
+        <Button variant="default" asChild className="hidden rounded-md lg:flex">
+          <Link href="/blog" className="text-base">
+            Blog
+          </Link>
         </Button>
         <ThemeToggle />
         <MobileMenu />
