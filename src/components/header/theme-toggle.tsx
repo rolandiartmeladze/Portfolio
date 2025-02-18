f5f21dc46c781f437814cfb3a4a4414bbe584922
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-18 13:06:49 -0800

    update theme toggle component remove switch / add icons

--- src/components/header/theme-toggle.tsx
+++ src/components/header/theme-toggle.tsx
@@ -1,18 +1,16 @@
 "use client";
 
 import { useTheme } from "@/components/theme-provider";
-import { Switch } from "@/components/ui/switch"; 
+import { Icons } from "../icons";
 
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
 
-  if (theme === null) return null; 
+  if (theme === null) return null;
 
   return (
-    <Switch
-      checked={theme === "dark"} 
-      onCheckedChange={toggleTheme}
-    >
-    </Switch>
+    <div className="flex z-20 cursor-pointer size-9 text-foreground hover:text-primary" onClick={toggleTheme}>
+      {theme === "dark" ?  <Icons.sun /> :<Icons.moon />}
+    </div>
   );
 }
