c95bc1787be9b31bd672fcefb71b2a1ae8a65a7d
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-18 13:05:37 -0800

    update contact buttons

--- src/components/header/contact-buttons.tsx
+++ src/components/header/contact-buttons.tsx
@@ -1,28 +1,23 @@
 import Link from "next/link";
 import { Icons } from "../icons";
-import { buttonVariants } from "../ui/button";
 
 export default function ContactButtons() {
   return (
-    <ul className="flex flex-row lg:flex-row">
-      <li>
-        <Link
-          className={buttonVariants({ variant: "link" }) + "px-0"}
-          href="mailto:example@email.com"
-        >
-          <Icons.gmail className="w-9 p-0" />
-          <label className="hidden lg:flex">Send Email</label>
-        </Link>
-      </li>
-      <li>
-        <Link
-          className={buttonVariants({ variant: "link" })}
-          href="tel:+995595035668"
-        >
-          <Icons.phone />
-          <label className="hidden lg:flex">Call Now</label>
-        </Link>
-      </li>
-    </ul>
+    <div className="flex lg:gap-2">
+      <Link
+        href="mailto:rartmeladze@gmail.com"
+        className="flex p-2 border-r rounded-s-md text-primary bg-muted items-center gap-2 lg:px-4 lg:py-2 lg:bg-secondary lg:border-r-0 lg:shadow-lg hover:bg-background hover:border-b border-primary"
+      >
+        <Icons.gmail className="size-6" />
+        <label className="hidden lg:flex cursor-pointer">Send Email</label>
+      </Link>
+      <Link
+        href="tel:+995595035668"
+        className="flex p-2 border-l text-primary bg-muted rounded-e-md items-center gap-2 lg:px-4 lg:py-2  lg:bg-secondary lg:border-l-0 lg:shadow-lg hover:bg-background hover:border-b border-primary"
+      >
+        <Icons.phone className="size-6" />
+        <label className="hidden lg:flex cursor-pointer">Call Now</label>
+      </Link>
+    </div>
   );
 }
