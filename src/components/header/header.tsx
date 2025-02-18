dcb4e38462f342a1d1fbddcea46162d43b56406c
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-18 13:19:43 -0800

    update header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     <header className="px-4 py-3 flex justify-between bg-secondary border-b border-borderColor lg:px-12">
       <ContactButtons />
       <div className="flex justify-between items-center  gap-2 lg:gap-3">
-        <Link href="/blog" className="hidden hover:bg-primary lg:flex lg:p-1 items-center lg:border-b lg:border-b-primary lg:rounded-md">
+        <Link href="/blog" className="hidden lg:bg-primary hover:bg-secondary lg:flex lg:p-1 items-center lg:border-b lg:border-b-foreground lg:rounded-md">
           <Icons.blog className="size-9 z-20  lg:size-7" />
         </Link>
         <ThemeToggle />
