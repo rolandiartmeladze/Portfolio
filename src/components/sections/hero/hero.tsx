78af9ff0035c0aa818a5da5b246eb574620fcd1d
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-28 01:25:05 -0800

    refstructure optimise code with dinamic Ui

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -1,17 +1,28 @@
-import TypingText from "@/components/typing-text";
+"use client";
+
+import TypingText from "@/components/ui/typing-text";
+import SliceText from "@/components/ui/slice-text";
+import CvButton from "./cv-button";
 
 export default function Hero() {
   return (
     <div className="flex w-full flex-col gap-3 text-center">
-      <span className="text-5xl text-foreground animate-pulse">Hi 👋 I’M</span>
-      <h1 className="text-6xl font-bold text-foreground animate-fadeIn">
-        <span className="text-primary">Roland Artmeladze</span>
-      </h1>
-      <h3 className="text-5xl font-bold">
-        <TypingText
-          text={["Full-Stack Developer.", "MERN-Stack Developer.", "Coder."]}
-        />
-      </h3>
+      <span className="text-5xl text-slate-600 animate-slideDown">
+        Hi
+        <samp className="animate-pulse">👋</samp>
+        I’M
+      </span>
+      <SliceText
+        text="Roland&nbsp;Artmeladze"
+        className="text-6xl font-bold text-primary"
+      />
+      <TypingText
+        className="text-5xl font-bold"
+        text={["Full-Stack Developer.", "MERN-Stack Developer.", "Coder."]}
+      />
+
+      <CvButton />
+      
     </div>
   );
 }
