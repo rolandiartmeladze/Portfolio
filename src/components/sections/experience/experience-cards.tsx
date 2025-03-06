59116f4c8e0195bc7902b87f594dde7a8ae7ab41
rolandiartmeladze <rartmeladze@gmail.com>
2025-03-07 02:57:45 +0400

    update small detalies

--- src/components/sections/experience/experience-cards.tsx
+++ src/components/sections/experience/experience-cards.tsx
@@ -33,20 +33,20 @@ export default function ExperienceCards() {
             </div>
           </div>
 
-          <div className={`${work.indicator}description p-3 mt-4 lg:p-12 gap-8 grid grid-cols-1 lg:grid-cols-3"`}>
-           
-           <div>              
-            <h3 className={`text-primary text-xl underline`} >Description:</h3>
+          <div
+            className={`${work.indicator}description p-3 mt-4 lg:p-12 gap-8 grid grid-cols-1 lg:grid-cols-3"`}
+          >
+            <div>
+              <h3 className="text-primary text-xl underline">Description:</h3>
 
-             <p
-              className={`text-foreground lg:col-span-2`}
-            >
-              {work.description}
-            </p>
+              <p className={`text-foreground lg:col-span-2`}>
+                {work.description}
+              </p>
             </div>
             <div
               className={`${work.indicator}skills mt-3 flex flex-wrap gap-2 lg:col-span-1`}
             >
+              <h3 className="text-foreground text-xl underline">Skills:</h3>
               {work.skills.map((skill, idx) => (
                 <span
                   key={idx}
