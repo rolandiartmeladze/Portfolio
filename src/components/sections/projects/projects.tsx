ddb1c35ff039ada795a24bf68ff7a4ca7f2e4795
rolandiartmeladze <rartmeladze@gmail.com>
2025-03-11 01:27:30 +0400

    update projects component

--- src/components/sections/projects/projects.tsx
+++ src/components/sections/projects/projects.tsx
@@ -6,20 +6,30 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Icons } from "@/components/icons";
+import ProjectCard from "./project-card";
 
 export default function Projects() {
   return (
-    <section className="container h-[98vh] lg:w-[96%]">
+    <section className="container h-[98vh] lg:w-[96%] flex flex-col">
       <SectionHeader label="Projects" className="mb-4">
-        <samp>4</samp>
+        <Icons.work className="size-5" />
       </SectionHeader>
 
       <Carousel>
         <CarouselContent>
-          <CarouselItem>...</CarouselItem>
-          <CarouselItem>... </CarouselItem>
-          <CarouselItem>...</CarouselItem>
-          <CarouselItem>...</CarouselItem>
+          <CarouselItem>
+            <ProjectCard />
+          </CarouselItem>
+          <CarouselItem>
+            <ProjectCard />
+          </CarouselItem>
+          <CarouselItem>
+            <ProjectCard />
+          </CarouselItem>
+          <CarouselItem>
+            <ProjectCard />
+          </CarouselItem>
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
