d560455fec15ad218e7279588d1b8edcd0ed4b23
rolandiartmeladze <rartmeladze@gmail.com>
2025-03-13 00:51:17 +0400

    create education section

---
+++ src/components/sections/education/education.tsx
@@ -0,0 +1,22 @@
+import SectionHeader from "@/components/ui/section-header";
+import { Icons } from "@/components/icons";
+import EducationCard from "./education-card";
+import { educationData } from "@/config/education";
+
+export default function Education() {
+  return (
+    <section
+      id="education"
+      className="container flex flex-col gap-4 h-screen w-[96%]"
+    >
+      <SectionHeader label="Education">
+        <Icons.education className="size-5" />
+      </SectionHeader>
+{
+    educationData.map((education) =>(
+              <EducationCard key={education.institution} education={education} />
+    ))
+}
+    </section>
+  );
+}
