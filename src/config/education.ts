c6d1da55bd891bfe4409973c548fd63abd4fc1c2
rolandiartmeladze <rartmeladze@gmail.com>
2025-03-13 00:52:24 +0400

    add aducation data

---
+++ src/config/education.ts
@@ -0,0 +1,22 @@
+import { education } from "@/types/education";
+
+export const educationData: education[] = [
+    {
+      "institution": "Codecademy",
+      "period": "02.2024 – 03.2024",
+      "course": "Learn Node.js / Learn Express",
+      "description": "Completed intensive courses on Node.js and Express.js, gaining hands-on experience in backend development. Learned to build RESTful APIs and manage server-side applications."
+    },
+    {
+      "institution": "CodeHS",
+      "period": "11.2023 – 12.2023",
+      "course": "JavaScript Programming",
+      "description": "Developed a strong foundation in JavaScript, focusing on problem-solving, algorithms, and interactive web applications."
+    },
+    {
+      "institution": "Georgian Technical University",
+      "period": "2015 – 2019",
+      "course": "Bachelor’s in Computer Engineering",
+      "description": "Gained comprehensive knowledge in computer engineering, covering software development, networking, and embedded systems."
+    }
+  ];
