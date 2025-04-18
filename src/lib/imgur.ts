97964b18e7a7ad8506f42cf0f2267766a81a8003
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-04-19 03:10:09 +0400

    add Fetch request Function with tacke Images from imgur Platform

---
+++ src/lib/imgur.ts
@@ -0,0 +1,17 @@
+const IMGUR_CLIENT_ID = '6c69e95f302c12b';
+
+export async function fetchImgurImages(albumHash: string) {    
+  const res = await fetch(`https://api.imgur.com/3/album/${albumHash}/images`, {
+    headers: {
+      Authorization: `Client-ID ${IMGUR_CLIENT_ID}`,
+    },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data?.data?.error || "Imgur fetch failed");
+  }
+
+  return data.data;
+}
