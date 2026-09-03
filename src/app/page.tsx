32e858db9f8ee215105864f6b06ba4db91f95969
Roland Artmeladze <rartmeladze@gmail.com>
2026-09-03 23:07:55 +0400

    resolve conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
 import Footer from "@/components/Footer";
-import Image from "next/image";
+import React from 'react';
+import Header from '@/components/Header';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="text-6xl font-bold text-black dark:text-white sm:text-5xl">
-          Portfolio is updating         
-        </h1>
+    <>
+      {/* Noise texture overlay */}
+      <div className="noise-overlay" aria-hidden="true" />
+      {/* Grid background lines */}
+      <div className="grid-bg-lines" aria-hidden="true">
+        <div className="grid-bg-inner">
+          <div className="grid-bg-line" />
+          <div className="grid-bg-line hidden md:block" />
+          <div className="grid-bg-line hidden lg:block" />
+          <div className="grid-bg-line" />
+        </div>
+      </div>
+
+      <Header />
+
+      <main className="relative z-10 h-screen flex flex-col items-center justify-center text-center px-4">
       </main>
 
       <Footer />
-    </div>
+    </>
   );
 }
